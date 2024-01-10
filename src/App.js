257b650cff8c49f459b317c2557c83db35a284d0
spontius415 <116679132+spontius415@users.noreply.github.com>
2024-01-10 12:09:28 -0500

    added search form to page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./Sarah Pontius-logos_black.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Sarah Pontius</footer>
+      </div>
     </div>
   );
 }
