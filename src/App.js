2064ba701381bf72225e1f959f40ef21697623b3
spontius415 <116679132+spontius415@users.noreply.github.com>
2024-01-10 11:11:02 -0500

    compiled project framing

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./Sarah Pontius-logos_black.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
